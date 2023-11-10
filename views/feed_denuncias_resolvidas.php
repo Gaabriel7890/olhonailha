dcc340682383202e2773300469e56c399b869e19
TheHecken <christianrocha009@hotmail.com>
2023-11-10 17:35:29 -0200

    Front end/C

---
+++ views/feed_denuncias_resolvidas.php
@@ -0,0 +1,20 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT'] . '/olhonailha/templates/_cabecalho.php';
+
+
+
+
+?>
+
+<div class="denunciasresolvidas">
+  <div class="titulo" style="background-color: green; color:white; text-align:center">
+<h1>Denúncias solucionadas</h1>
+  </div>  
+  <div class="postagem">
+      
+  </div>
+
+
+
+
+</div>
