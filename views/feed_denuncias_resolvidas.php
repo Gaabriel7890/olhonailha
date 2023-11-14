ff3b7b60af3b12560e72f1dcd28b24495374044c
TheHecken <christianrocha009@hotmail.com>
2023-11-14 17:39:13 -0200

    update

--- views/feed_denuncias_resolvidas.php
+++ views/feed_denuncias_resolvidas.php
@@ -6,15 +6,22 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/olhonailha/templates/_cabecalho.php';
 
 ?>
 
-<div class="denunciasresolvidas">
+<section class="denunciasresolvidas">
   <div class="titulo" style="background-color: green; color:white; text-align:center">
 <h1>Denúncias solucionadas</h1>
   </div>  
   <div class="postagem">
       
   </div>
+  <?php foreach ($denuncias as $d): ?>
+    <?= $d['id_usario'] ?>
+    <?= $d['id denuncia'] ?>
+    <?= $p['id_tipo'] ?>
+    <?= $p['titulo'] ?>
+    <?= $p['local_denuncia'] ?>
+    <?= $p['descricao'] ?>
+    <?= $p['foto_denuncia'] ?>
 
 
 
-
-</div>
+</section>
